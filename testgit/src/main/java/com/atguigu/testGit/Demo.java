298cc4c052b6af25856702e2d0c03d2d179e11ad
atguigu <13590149083@163.com>
2020-09-07 16:39:50 +0800

    第一次更新

--- testgit/src/main/java/com/atguigu/testGit/Demo.java
+++ testgit/src/main/java/com/atguigu/testGit/Demo.java
@@ -3,5 +3,6 @@ package com.atguigu.testGit;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("这是一个git测试项目");
+        System.out.println("我进行了一些改变");
     }
 }
